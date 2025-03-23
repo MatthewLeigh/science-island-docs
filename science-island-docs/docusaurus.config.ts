c0898e66daa43f5b82cbec5fe01084c2c6d53604
MatthewLeigh <matthewcross97@gmail.com>
2025-03-23 21:34:07 +1100

    Reconfigure with initial doc structure

--- science-island-docs/docusaurus.config.ts
+++ science-island-docs/docusaurus.config.ts
@@ -2,8 +2,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'Science Island',
   tagline: 'Cool, Fun & Engaging',
@@ -11,21 +9,16 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://scienceisland.com/#/',
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Education Network Group', // Usually your GitHub org/user name.
-  projectName: 'Science Island', // Usually your repo name.
-
+  organizationName: 'Education Network Group',
+  projectName: 'Science Island',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -52,12 +45,11 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/science-island-social-card.png',
     navbar: {
       title: 'Science Island Docs',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Science Island Logo',
         src: 'img/logo.png',
       },
       items: [
@@ -75,8 +67,8 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Docs',
+              to: '/',
             },
           ],
         },
@@ -84,16 +76,8 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
+              href: 'https://discord.com/channels/1350984587148525579/1350984587664294005',
             },
           ],
         },
