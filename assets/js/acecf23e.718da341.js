0e244aca10c5ba445021f274f87b474ffb12b0bf
MatthewLeigh <MatthewLeigh@users.noreply.github.com>
2025-04-01 09:22:12 +0000

    deploy: 8ba30a9518969c7c2c8ce89180f56d48daf555ca

---
+++ assets/js/acecf23e.718da341.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkscience_island_docs=self.webpackChunkscience_island_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/science-island-docs/projects","blogTitle":"Student Projects","authorsListPath":"/science-island-docs/projects/authors"}')}}]);
